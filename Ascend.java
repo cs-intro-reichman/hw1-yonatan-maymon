cbbc6e0eef09070b77dc05701ff209f73fae0dfd
unknown <yonatan.maymon@post.runi.ac.il>
2025-11-03 12:56:15 +0200

    correction to Ascend

--- Ascend.java
+++ Ascend.java
@@ -3,13 +3,13 @@
 public class Ascend {
 	public static void main(String[] args) {
 		int limit = Integer.parseInt(args[0]);
-		int a = (int) ((Math.random() * limit) + 1) ;
-		int b = (int) ((Math.random() * limit) + 1) ;
-		int c = (int) ((Math.random() * limit) + 1) ;
+		int a = (int) ((Math.random() * limit)) ;
+		int b = (int) ((Math.random() * limit)) ;
+		int c = (int) ((Math.random() * limit)) ;
 		System.out.println( a + " " + b + " " + c);
 		int minNumber = Math.min(Math.min(a, b), c);
 		int maxNumber = Math.max(Math.max(a, b), c);
-		int middleNumber = Math.min(Math.max(a, b), Math.max(b, c));
+		int middleNumber = Math.min(Math.min(Math.max(a, b), Math.max(b, c)), Math.max(a,c));
 		System.out.println( minNumber + " " + middleNumber + " " + maxNumber);
 	}
 }
